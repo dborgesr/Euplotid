0b551fbcf98520aecabd5fb802053efac3fec584
Diego Borges <dborgesr@mit.edu>
2017-07-20 21:00:55 -0400

    fixing image

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-modprobe i2c-dev #sensehat
-cd /usr/src/app/RPi_Cam_Web_Interface && service nginx restart #microscope server
-#modprobe bcm2835-v4l2 #raspicam
-
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 sleep 1
@@ -11,7 +7,3 @@ cd /usr/src/app/ && node resin-wifi-connect/src/app.js --clear=true
 
 cd /root/Euplotid
 jupyter notebook --allow-root --port=80 --no-browser --ip=0.0.0.0
-
-while true; do
-	sleep 100
-done
