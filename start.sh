580677ffa6b197227fec4b2c2bfd33536f90fbc4
Diego Borges <dborgesr@mit.edu>
2017-11-25 20:21:22 -0500

    changed start

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 sleep 1
-cd /usr/src/app/ && node resin-wifi-connect/src/app.js --clear=true
+cd /usr/src/app/ && node resin-wifi-connect/src/app.js --clear=false
 
 cd /root/Euplotid
 jupyter notebook --allow-root --port=80 --no-browser --ip=0.0.0.0
