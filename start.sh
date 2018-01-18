88aca3bc194e0c22ee8a3d0d90706c1d5f4efe6b
Diego Borges <dborgesr@mit.edu>
2018-01-17 22:21:15 -0500

    DF

--- start.sh
+++ start.sh
@@ -17,4 +17,4 @@ cd /usr/src/app/ && ./wifi-connect --clear=false
 cd /root/Euplotid/euplotid && python applotid.py &
 #Start jupyter
 cd /root/Euplotid
-jupyter notebook --allow-root --port=80 --no-browser --ip=0.0.0.0
+jupyter notebook --allow-root --port=8888 --no-browser --ip=0.0.0.0
