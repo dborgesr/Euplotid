b0d2e0e7f1c0a74d4174b0a42d5b11c5a64fbf00
Diego Borges <dborgesrmit@gmail.com>
2020-04-01 14:44:45 -0400

    Updating Euplotid image

---
+++ EuplotidDockerImage/start.sh
@@ -0,0 +1,20 @@
+# Logic on container startup depends on a few pre-defined image variables
+
+#Activate default environment
+source activate env
+
+# Deploy euplotid app, few scenarios depending on:
+# OS: [debian, alpine, ubuntu]
+# Arch: [armv6, armv7, aarch64, x86]
+# Deploy: [true, false]
+
+#If deploying w/ frontend or not
+if [ "${EUPLO_DEPLOY:-}" == "true" ] ; then 
+	python3 /app/applotid.py 
+fi
+
+#Run jupyter as IDE
+if [ "${EUPLO_DEPLOY:-}" == "false" ] ; then
+	jupyter lab --allow-root --port=$JUPYTER_PORT --no-browser --ip=0.0.0.0
+fi
+
