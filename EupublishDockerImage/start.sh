d1515856339028c1ba1b1dad1db1b9b26d63bf1a
Diego <dborgesrmit@gmail.com>
2020-05-02 14:32:01 -0400

    updating eupublish

---
+++ EupublishDockerImage/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Activate default environment
+source activate base
+
+# Deploy euplotid app, few scenarios depending on:
+# OS: [debian, alpine, ubuntu]
+# Arch: [armv6, armv7, aarch64, x86]
+# Deploy: [true, false]
+
+#If deploying w/ frontend or not
+if [ "${EUPLO_DEPLOY:-}" == "true" ] ; then
+	python3 /app/applotid.py
+	#bundle exec jekyll serve
+fi
+
+#Run jupyter as IDE
+if [ "${EUPLO_DEPLOY:-}" == "false" ] ; then
+	jupyter lab --allow-root --port=$JUPYTER_PORT --no-browser --ip=0.0.0.0
+fi
